de49889d7c727bc89cc819492bc4cc1720316ccc
chahxt <165466251+chahxt@users.noreply.github.com>
2025-02-06 22:24:46 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,5 @@
+try:
+    import torch.nn
+    print("torch.nn is available!")
+except ImportError:
+    print("torch.nn is not available!")
---
+++ autoencoder.py
@@ -0,0 +1,25 @@
+# models/autoencoder.py
+
+import torch
+import torch.nn as nn
+
+class Autoencoder(nn.Module):
+    def __init__(self):
+        super(Autoencoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=3, stride=2, padding=1),  # Conv layer to downsample
+            nn.ReLU(),
+            nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1),
+            nn.ReLU()
+        )
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(32, 16, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(16, 3, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.Sigmoid()  # Use Sigmoid to normalize outputs between [0, 1]
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
